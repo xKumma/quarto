ad8c223fc03813eddea732baafa43322adaac0ff
hartemoira <moira.harte@student.kdg.be>
2025-03-10 11:31:12 +0100

    update MainMenu view package (presenter and view)

--- src/be/kdg/integration2/mvpglobal/view/mainmenu/MainMenuPresenter.java
+++ src/be/kdg/integration2/mvpglobal/view/mainmenu/MainMenuPresenter.java
@@ -2,14 +2,43 @@ package be.kdg.integration2.mvpglobal.view.mainmenu;
 
 import be.kdg.integration2.mvpglobal.model.MVPModel;
 import be.kdg.integration2.mvpglobal.view.UISettings;
+import be.kdg.integration2.mvpglobal.view.gamesetupscreen.GameSetupView;
 import be.kdg.integration2.mvpglobal.view.startscreen.StartScreenView;
 import javafx.scene.*;
 
 public class MainMenuPresenter {
 
     private MVPModel model;
-    private StartScreenView view;
+    private MainMenuView view;
     private UISettings uiSettings;
+
+    public MainMenuPresenter(MainMenuView view, MVPModel model, UISettings uiSettings) {
+        this.model = model;
+        this.view = view;
+        this.uiSettings = uiSettings;
+    }
+
+    protected void addEventHandlers() {
+        view.getQuitButton().setOnAction(e -> quitGame());
+        view.getLeaderboardButton().setOnAction(e ->  goToLeaderboard());
+        view.getRulesButton().setOnAction(e -> goToRules());
+        view.getStartGameButton().setOnAction(e -> goToGameSetup());
+    }
+
+    private void quitGame() {
+
+    }
+    private void goToLeaderboard() {
+
+    }
+    private void goToRules() {
+
+    }
+    private void goToGameSetup() {
+
+    }
+
+
 
 
 }
