7bf52b7ae248f5e104b92b9fc5753a8593d0ced6
plamen <plamen.hristov@student.kdg.be>
2025-04-03 14:58:06 +0200

    Removed POC package

--- src/poc/InvalidMoveException.java
+++
@@ -1,8 +0,0 @@
-package poc;
-
-public class InvalidMoveException extends RuntimeException {
-    public InvalidMoveException(Move move) {
-      super(String.format("Invalid move by %s!!! (Pos: %d:%d )",
-              move.getPlayer().name , move.getPosition().x(), move.getPosition().y()));
-    }
-}
